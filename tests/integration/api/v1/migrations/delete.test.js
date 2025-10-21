f2006d6e92ae8f1d941c489780d9f4e3d02cbca7
Marco Belo <marco.barone.belo@gmail.com>
2025-10-21 01:11:54 +0000

    removendo database no teste DELETE em `/migrations` endpoint

--- tests/integration/api/v1/migrations/delete.test.js
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -1,16 +1,7 @@
-import database from "infra/database.js";
-
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
-  await database.query("drop schema public cascade; create schema public;");
-}
-
 test("DELETE to /api/v1/migrations should return 405", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "DELETE",
   });
-
   expect(response.status).toBe(405);
   const responseBody = await response.json();
   expect(responseBody).toEqual({ error: 'Method "DELETE" not allowed' });
